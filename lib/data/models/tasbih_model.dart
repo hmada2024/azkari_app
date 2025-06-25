73a686671b7daeeb3321454d96b8a7695464de53
hmada2024 <alasuity881991@gmail.com>
2025-06-25 06:51:00 +0300

    8/

--- lib/data/models/tasbih_model.dart
+++ lib/data/models/tasbih_model.dart
@@ -4,19 +4,21 @@ class TasbihModel {
   final int id;
   final String text;
   final int sortOrder;
+  final bool isDeletable;
 
   TasbihModel({
     required this.id,
     required this.text,
     required this.sortOrder,
+    required this.isDeletable,
   });
 
-  // Factory constructor لإنشاء نسخة من الخريطة (صف قاعدة البيانات)
   factory TasbihModel.fromMap(Map<String, dynamic> map) {
     return TasbihModel(
       id: map['id'],
       text: map['text'],
       sortOrder: map['sort_order'],
+      isDeletable: map['is_deletable'] == 1, 
     );
   }
 }
